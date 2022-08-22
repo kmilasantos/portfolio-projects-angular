8968f291f3293864a0e969596bae9182901e49f0
kmilasantos <csilva.santos1997@gmail.com>
2022-08-22 12:05:23 -0400

    alterando nome do módulo header

--- landing-page-nature/src/app/app.module.ts
+++ landing-page-nature/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
